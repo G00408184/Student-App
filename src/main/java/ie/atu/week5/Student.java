231bb853119ac4fd2ae6558ec767538828092992
Jakub Mamala <G00408184@atu.ie>
2022-10-24 09:56:42 +0100

    Prompt user for Name

--- src/main/java/ie/atu/week5/Student.java
+++ src/main/java/ie/atu/week5/Student.java
@@ -1,7 +1,14 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class Student {
     public static void main(String[] args) {
+        System.out.println("Please enter Student Name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("you entered :" + name);
+
 
 
 
