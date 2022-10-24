ef1045101145a15dc3e62b955315b7fd11c5d210
Jakub Mamala <G00408184@atu.ie>
2022-10-24 10:14:11 +0100

    Added new file called StudentStore

---
+++ src/main/java/ie/atu/week5/StudentStore.java
@@ -0,0 +1,31 @@
+package ie.atu.week5;
+
+public class StudentStore {
+    private String name;
+    private String email;
+    private String course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
